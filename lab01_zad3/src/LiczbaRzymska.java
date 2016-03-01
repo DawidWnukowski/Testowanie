73f383d4336e767fd5f127ddcdd110d2d6858948
Vuko <dawidwdvd@yahoo.com>
2016-03-01 12:00:42 +0100

    The end

--- lab01_zad3/src/LiczbaRzymska.java
+++ lab01_zad3/src/LiczbaRzymska.java
@@ -7,4 +7,24 @@ public class LiczbaRzymska
     {
         this.a = a;
     }
+
+    @Override
+    public String toString()
+    {
+        String str = "";
+        int val;
+
+        if(a >= 2000)
+        {
+            a-=2000;
+            str+="MM";
+        }
+        if(a >= 1500)
+        {
+            a-=1500;
+            str+="MD";
+        }
+
+        return str;
+    }
 }
